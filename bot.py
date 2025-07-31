ae711345ad11eeda1a9cbcadf6c6e0614842aa40
jannilsoncunha <jannilsoncunha@gmail.com>
2025-07-31 04:24:53 -0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -614,6 +614,8 @@ if __name__ == '__main__':
         asyncio.run(main())
     except RuntimeError as e:
         if 'event loop' in str(e):
+            import nest_asyncio
+            nest_asyncio.apply()
             loop = asyncio.get_event_loop()
             loop.run_until_complete(main())
         else:
